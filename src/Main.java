7297b4a8b639057554fa1ea2bf2bd5d58c942fef
NikolajPedersen29 <mejlby2002@hotmail.com>
2025-05-06 10:42:53 +0200

    CurrencyRepository Done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 import repository.StockRepository;
-import repository.TransactionRepository;
 import repository.UserRepository;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Osmans Disciple!");
 
-//        UserRepository.readUserFile();
-//        StockRepository.readStockFile();
-        TransactionRepository.readTransactionFile();
+        UserRepository.readUserFile();
+        StockRepository.readStockFile();
     }
 }
--- src/repository/CurrencyRepository.java
+++ src/repository/CurrencyRepository.java
@@ -1,4 +1,66 @@
 package repository;
 
+import Objects.Currency;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CurrencyRepository {
+    public static List<Currency> getAllCurrencies() {
+        List<Currency> currencies = new ArrayList<>();
+
+        try {
+            File currencyFile = new File("src/files/currency.csv");
+            Scanner reader = new Scanner(currencyFile);
+
+            boolean isFirstLine = true;
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+
+                String[] data = line.split(";");
+
+                try {
+                    String baseCurrency = data[0].trim();
+                    String quoteCurrency = data[1].trim();
+                    double rate = Double.parseDouble(data[2].trim().replace(",", "."));
+                    LocalDate lastUpdated = LocalDate.parse(data[3].trim(), DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+
+                    Currency currency = new Currency(baseCurrency, quoteCurrency, rate, lastUpdated);
+                    currencies.add(currency);
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Kan ikke aflæse: " + line);
+
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Filen blev ikke fundet!");
+        }
+        return currencies;
+    }
+
+
+
+    public static Currency findByBaseCurrency(String baseCurrency) {
+        for (Currency currency : getAllCurrencies()) {
+            if (currency.getBaseCurrency().equalsIgnoreCase(baseCurrency)) {
+                return currency;
+            }
+        }
+        return null;
+
+    }
+
 }
